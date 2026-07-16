11b6f28faf58a728ae7af5abb2500bff634bd400
tam13062001 <dtam768@gmail.com>
2026-07-16 12:31:32 +0700

    Fix exact file casing for topPostController

--- backend/src/routes/ToppostsRoute.js
+++ backend/src/routes/ToppostsRoute.js
@@ -1,6 +1,6 @@
 // routes/TopPosts.js
 import express from 'express';
-import { getCachedTopPosts, refreshTopPostsFromDb } from '../controllers/toppostcontroller.js';
+import { getCachedTopPosts, refreshTopPostsFromDb } from '../controllers/temp.js';
 
 const router = express.Router();
 
