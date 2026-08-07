4f4a6389900d4f9ab19bdb42a15506faba73da8d
tam13062001 <dtam768@gmail.com>
2026-08-07 08:16:28 +0700

    be 1

--- backend/src/server.js
+++ backend/src/server.js
@@ -24,7 +24,7 @@ app.use(cors({
   origin: [
     'https://client.100xmedia.agency',
     'https://thegivecollective.vercel.app',  
-    'http://localhost:5173',                  
+    'http://localhost:5173',           
     'http://localhost:3000',
     'https://analytics.thegivecollective.com',
   ],
